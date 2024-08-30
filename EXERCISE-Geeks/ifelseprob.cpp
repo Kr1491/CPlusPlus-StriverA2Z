95ea6bd9938bbb319a15faac6eed68ffc9eec365
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-30 22:16:31 +0530

    Prob4 Complete

---
+++ EXERCISE-Geeks/ifelseprob.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n = 8;
+  if (n>5)
+  {
+    cout<<"Greater than 5";
+  }
+  else if (n<5)
+  {
+    cout<<"Less than 5";
+  }
+  
+  else if (n==5)
+  {
+    cout<<"Equal to 5";
+  }
+  
+}
