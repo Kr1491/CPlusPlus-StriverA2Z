3d44075b773191c230f97ba67ffeca10ec637825
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-31 21:48:18 +0530

    OperatorsProblem

---
+++ EXERCISE-Geeks/operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int cppOperators(int A, int B){
+    int sum = A+B;
+    int multiply = A*B;
+
+    if (A>=B)
+    {
+        int divide = A/B;
+        int subtract = A-B;
+    }
+
+    else{
+        int divide = B/A;
+        int subtract = B-A;
+    }
+    
+    
+
+}
