9d98a077401f3d00dba0d6deebeb8de92d1d492c
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-31 22:24:39 +0530

    Datatype Problem

---
+++ EXERCISE-Geeks/datatype.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+// #include <typeinfo>
+// using namespace std;
+
+// int datatype(string s){
+//     char i = 'i';
+//     if (typeid(s).name() == i)
+//     {
+//         /* code */
+//     }
+    
+// }
+// int main(){
+
+// }
