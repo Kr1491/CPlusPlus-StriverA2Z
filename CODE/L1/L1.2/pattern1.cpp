3556015b4a9d816837616b81a576e53bb5ee459a
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-06 10:47:31 +0530

    L1.2 Pattern Exercise

---
+++ CODE/L1/L1.2/pattern1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void nForest(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+        
+    }
+    
+}
