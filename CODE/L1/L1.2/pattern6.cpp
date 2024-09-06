d72eef6a4fa6e5a5c001129c67f2f9dc25efee72
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-06 11:12:47 +0530

    Pattern 6

---
+++ CODE/L1/L1.2/pattern6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void nNumberTriangle(int n) {
+    for (n; n >= 1; n--)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+}
