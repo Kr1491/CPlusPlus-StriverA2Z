baffeb5ac4d6f5bc0820f8678f647e9e42f25adc
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-07 12:52:53 +0530

    Prob6

---
+++ CODENINJA/prb6.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, power;
+    int out = 1;
+    cout << ">>";
+    cin >> num >> power;
+
+    if (num == 0 && power == 0)
+    {
+        cout << 0;
+    }
+
+    else if (num != 0 && power == 0)
+    {
+        cout << num;
+    }
+
+    else
+    {
+        for (int i = 1; i <= power; i++)
+        {
+            out = out * num;
+        }
+        cout << out;
+    }
+
+    return 0;
+}
