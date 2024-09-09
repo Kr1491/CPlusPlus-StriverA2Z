74dc357085a9ba87a5a9d7c944390d0f3a638402
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-09 13:47:20 +0530

    Updat1

---
+++ CODENINJA/practice_problems/ppb7.cpp
@@ -0,0 +1,30 @@
+// #include <iostream> //code error
+// using namespace std;
+
+// void nStarTriangle(int n)
+// {
+//     for (int i = 1; i <= (n - 1) / 2; i++)
+//     {
+//         for (int l = 0; l < i; l++)
+//         {
+//             cout << " ";
+//             n--;
+//         }
+//         for (int k = 1; k <= n; k++)
+//         {
+//             for (int j = 1; j <= k; j++)
+//             {
+//                 cout << "*";
+//             }
+
+//             cout << endl;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     nStarTriangle(n);
+// }
