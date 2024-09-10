e8385d1589d96dbcbd48f5a24225c7f09f7d224c
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-10 09:57:02 +0530

    Prob8 cOMP

---
+++ CODENINJA/practice_problems/ppb8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<">>";
+    cin>>n;
+
+
+    for (int i = 0; i < n; i++) //outer loop
+    {
+        for (int j = 0; j <= n-i-1; j++){
+            cout<<" ";
+        }
+
+        for (int k = 0; k < (2*i)-1; k++){
+            cout<<"*";
+        }
+
+        for (int j = 0; j <= n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+
+    }
+    
+}
