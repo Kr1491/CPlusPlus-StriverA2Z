d21200b58d86e85875b036fac23a7d99735fbf31
Krishna Jha <jhakrishna1491@gmail.com>
2024-09-17 19:40:11 +0530

    update

---
+++ CODENINJA/practice_problems/prb9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+void nStarTriangle(int n) {
+    for (int i = 0; i > n-i-1; i++)
+    {
+        cout<<i<<endl;
+    }
+}
+
+int main(){
+    nStarTriangle(4);
+}
